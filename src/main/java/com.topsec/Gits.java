c34bf08de79ff08ad72060655b84fb62bed7e206
lixupeng <li_xupeng@topsec.com.cn>
2024-05-28 18:09:18 +0800

    测试

--- src/main/java/com.topsec/Gits.java
+++ src/main/java/com.topsec/Gits.java
@@ -2,13 +2,7 @@ package com.topsec;
 
 public class Gits {
     public static void main(String[] args) {
-        System.out.println(111);
-        System.out.println(222);
-        System.out.println("devvvvv");
-        System.out.println(444);
-        System.out.println(5555);
-        System.out.println("这是dev");
-        System.out.println("pro");
+
         System.out.println("pro0516");
     }
 }
