337f788069862ded19baa936baed6874005004b9
lixupeng <li_xupeng@topsec.com.cn>
2024-11-28 10:51:58 +0800

    tag

--- src/main/java/com.topsec/Gits.java
+++ src/main/java/com.topsec/Gits.java
@@ -3,7 +3,6 @@ package com.topsec;
 public class Gits {
     public static void main(String[] args) {
 
-        System.out.println("dev tag111pro");
-        System.out.println("dev tag1111");
+        System.out.println("tag");
     }
 }
